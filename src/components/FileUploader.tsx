54ec9f250eb74c35723ca6fae9fa77ac3e0001fc
kjkurien <kjkurien@gmail.com>
2024-05-20 11:19:38 +0000

    File upload

--- src/components/FileUploader.tsx
+++ src/components/FileUploader.tsx
@@ -3,6 +3,8 @@ import { useState, useRef } from "react"
 import AttachmentIcon from "@mui/icons-material/Attachment"
 import DeleteIcon from "@mui/icons-material/Delete"
 import "./FileUploader.css"
+import axios from "axios"
+import { ArrowUpward } from "@mui/icons-material"
 
 const FileUploader = (props : any) => {
 
@@ -23,6 +25,23 @@ const FileUploader = (props : any) => {
         setFile(null);
         onDeleteFile();
     }
+    const handleSubmit = (event: any) => {
+        event.preventDefault()
+        const url = 'https://8r8zgngv90.execute-api.ap-south-1.amazonaws.com/test/test-hospital/occupancylist.xlsx';
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('fileName', file.name);
+        const config = {
+          headers: {
+            'Content-Type': 'application/vnd.ms-excel',
+            'Accept': "*/*"
+          },
+        };
+        axios.put(url, formData, config).then((response) => {
+          console.log(response.data);
+        });
+    
+      }
     return (
         <div className="file-uploader">
             <div className={'file-div ${disabled && "disabled"}'}>
@@ -37,6 +56,11 @@ const FileUploader = (props : any) => {
             <div className={'${disabled && "disabled"}'}>
                 <IconButton className="delete-icon" onClick={handleDeleteFile} disabled={disabled}>
                     <DeleteIcon />
+                </IconButton>
+            </div>
+            <div className={'${disabled && "disabled"}'}>
+                <IconButton className="delete-icon" onClick={handleSubmit} disabled={disabled}>
+                    <ArrowUpward />
                 </IconButton>
             </div>
         </div>
